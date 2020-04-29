2f2070ef0bc8b087790dd2c56697ff76dfceb5e0
uptophnarita <hiro.narita@gmail.com>
2020-04-29 14:35:47 -0400

    create user and save

--- index.js
+++ index.js
@@ -3,23 +3,8 @@ const app = express();
 const Sequelize = require('sequelize');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const sequelize = new Sequelize('postgres', 'postgres', process.env.POSTGRES_PW, {
-	host: 'localhost',
-	dialect: 'postgres',
-	port: process.env.POSTGRES_PORT
-});
-
-exports.sequelize = sequelize;
-
-sequelize
-	.authenticate()
-	.then(() => console.log('Connection has been established successfully.'))
-	.catch(err => console.error('Unable to connect to the database:', err));
-
-const User = require('./models/User').User;
-
-User.sync();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,7 +13,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.post('/register', (req, res) => console.log(req.body));
+exports.app = app;
+
+const sequelize = new Sequelize('postgres', 'postgres', process.env.POSTGRES_PW, {
+	host: 'localhost',
+	dialect: 'postgres',
+	port: process.env.POSTGRES_PORT
+});
+exports.sequelize = sequelize;
+
+const User = require('./models/User').User;
+User.sync({ force: true });
+
+passport.use('signup', new LocalStrategy({ passReqToCallback: true }, (_req, username, password, done) => {
+	User.findOne({ where: { username } }).then(user => {
+		if (user) { return done(null, false); }
+		else {
+			User.create({ username, password });
+		}
+	});
+}));
+
+app.post('/register', passport.authenticate('signup', {
+	successRedirect: '/',
+	failureRedirect: '/',
+	failureFlash: true
+}));
 
 app.listen(process.env.PORT);
