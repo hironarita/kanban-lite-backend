62b1bb7a456521a75b3827cae36f50c3c500e2f2
uptophnarita <hiro.narita@gmail.com>
2020-05-05 12:56:37 -0400

    move column endpoint

--- routes/columns.js
+++ routes/columns.js
@@ -6,23 +6,42 @@ const checkAuth = (req, res, next) => req.isAuthenticated() === true
     : res.status(401).send();
 
 router.get('/', checkAuth, (req, res) => {
-    Column.findAll({ where: { user_id: req.user.dataValues.id } }).then(columns => res.send(columns));
+    Column
+        .findAll({ where: { user_id: req.user.dataValues.id } })
+        .then(columns => res.send(columns));
 });
 
 router.post('/create', checkAuth, (req, res) => {
-    Column.create({
-        title: req.body.title,
-        boardIndex: req.body.boardIndex,
-        user_id: req.user.dataValues.id 
-    }).then(() => res.send());
+    Column
+        .create({
+            title: req.body.title,
+            boardIndex: req.body.boardIndex,
+            user_id: req.user.dataValues.id
+        })
+        .then(() => res.send());
 });
 
 router.post('/update', checkAuth, (req, res) => {
-    Column.findOne({ where: { id: req.body.id } }).then(async column => {
-        column.title = req.body.title;
-        await column.save();
-        res.send();
-    });
+    Column
+        .findOne({ where: { id: req.body.id } })
+        .then(async column => {
+            column.title = req.body.title;
+            await column.save();
+            res.send();
+        });
+});
+
+router.post('/move', checkAuth, (req, res) => {
+    Column
+        .findAll({ where: { user_id: req.user.dataValues.id } })
+        .then(columns => {
+            for (let i = 0; i < columns.length; i++) {
+                columns[i].boardIndex = req.body[columns[i].id];
+                columns[i].save();
+            }
+
+            res.send();
+        });
 });
 
 module.exports = router;
