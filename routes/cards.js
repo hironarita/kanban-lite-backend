85c95bb25a2f0e4afd1f2b525ebb49db629eeeef
uptophnarita <hiro.narita@gmail.com>
2020-05-11 11:35:51 -0400

    async await

--- routes/cards.js
+++ routes/cards.js
@@ -6,47 +6,49 @@ const checkAuth = (req, res, next) => req.isAuthenticated() === true
     ? next()
     : res.status(401).send();
 
-router.get('/', checkAuth, (req, res) => {
+router.get('/', checkAuth, async (req, res) => {
     if (req.query.columnIds.length === 0) return res.send([]);
 
     const colIds = req.query.columnIds.split(',');
-    Card.findAll({
+    const cards = await Card.findAll({
         where: {
             column_id: { [Op.in]: colIds }
         }
-    }).then(cards => res.send(cards));
+    });
+    res.send(cards);
 });
 
-router.post('/create', checkAuth, (req, res) => {
-    Card
-        .create({
-            title: req.body.title,
-            columnIndex: req.body.columnIndex,
-            column_id: req.body.columnId
-        })
-        .then(() => res.send());
+router.get('/card/:id', checkAuth, async (req, res) => {
+    const card = await Card.findOne({ where: { id: req.params.id } });
+    res.send(card);
 });
 
-router.post('/move', checkAuth, (req, res) => {
+router.post('/create', checkAuth, async (req, res) => {
+    await Card.create({
+        title: req.body.title,
+        columnIndex: req.body.columnIndex,
+        column_id: req.body.columnId
+    })
+    res.send();
+});
+
+router.post('/move', checkAuth, async (req, res) => {
     const cardIds = req.body.cardData.map(x => x.id);
-    Card
-        .findAll({
-            where: {
-                id: { [Op.in]: cardIds }
-            }
-        })
-        .then(cards => {
-            for (let i = 0; i < cards.length; i++) {
-                const card = req.body.cardData.find(x => x.id === cards[i].id);
-                if (card) {
-                    cards[i].column_id = card.column_id;
-                    cards[i].columnIndex = card.columnIndex;
-                    cards[i].save();
-                }
-            }
+    const cards = await Card.findAll({
+        where: {
+            id: { [Op.in]: cardIds }
+        }
+    });
+    for (let i = 0; i < cards.length; i++) {
+        const card = req.body.cardData.find(x => x.id === cards[i].id);
+        if (card) {
+            cards[i].column_id = card.column_id;
+            cards[i].columnIndex = card.columnIndex;
+            await cards[i].save();
+        }
+    }
 
-            res.send();
-        });
+    res.send();
 });
 
 module.exports = router;
--- routes/columns.js
+++ routes/columns.js
@@ -6,51 +6,41 @@ const checkAuth = (req, res, next) => req.isAuthenticated() === true
     ? next()
     : res.status(401).send();
 
-router.get('/', checkAuth, (req, res) => {
-    Column
-        .findAll({ where: { user_id: req.user.dataValues.id } })
-        .then(columns => res.send(columns));
+router.get('/', checkAuth, async (req, res) => {
+    const columns = await Column.findAll({ where: { user_id: req.user.dataValues.id } })
+    res.send(columns);
 });
 
-router.post('/create', checkAuth, (req, res) => {
-    Column
-        .create({
-            title: req.body.title,
-            boardIndex: req.body.boardIndex,
-            user_id: req.user.dataValues.id
-        })
-        .then(() => res.send());
+router.post('/create', checkAuth, async (req, res) => {
+    await Column.create({
+        title: req.body.title,
+        boardIndex: req.body.boardIndex,
+        user_id: req.user.dataValues.id
+    });
+    res.send();
 });
 
-router.post('/update', checkAuth, (req, res) => {
-    Column
-        .findOne({ where: { id: req.body.id } })
-        .then(async column => {
-            column.title = req.body.title;
-            await column.save();
-            res.send();
-        });
+router.post('/update', checkAuth, async (req, res) => {
+    const column = await Column.findOne({ where: { id: req.body.id } })
+    column.title = req.body.title;
+    await column.save();
+    res.send();
 });
 
-router.post('/move', checkAuth, (req, res) => {
-    Column
-        .findAll({ where: { user_id: req.user.dataValues.id } })
-        .then(async columns => {
-            for (let i = 0; i < columns.length; i++) {
-                columns[i].boardIndex = req.body[columns[i].id];
-                await columns[i].save();
-            }
+router.post('/move', checkAuth, async (req, res) => {
+    const columns = Column.findAll({ where: { user_id: req.user.dataValues.id } })
+    for (let i = 0; i < columns.length; i++) {
+        columns[i].boardIndex = req.body[columns[i].id];
+        await columns[i].save();
+    }
 
-            res.send();
-        });
+    res.send();
 });
 
 router.post('/delete/:id', checkAuth, async (req, res) => {
-    await Card.destroy({ where: { column_id: req.params.id } })
-
-    Column
-        .destroy({ where: { id: req.params.id } })
-        .then(() => res.send());
+    await Card.destroy({ where: { column_id: req.params.id } });
+    await Column.destroy({ where: { id: req.params.id } });
+    res.send();
 });
 
 module.exports = router;
